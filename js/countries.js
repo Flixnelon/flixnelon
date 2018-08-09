6075a54539a37d1c7c01ca02f65e96dcf2ea87c6
Flixnelon <flixnelon97@gmail.com>
2018-08-09 23:36:54 +0100

    added US + fixes

--- js/countries.js
+++ js/countries.js
@@ -5,113 +5,175 @@ $(document).ready(function () {
             checkAnswer();
         }
     });
-    /*
-        $('#capital_input').popover({
-            content: '<i class="fa fa-times"></i>',
-            html: true,
-            placement: 'right'
-        });
-    */
 });
 
-var _europe = {
+var _northcentralAmerica = {
 
-    "Albânia": "tirana",
-    "Alemanha": "berlim",
-    "Andorra": "andorra a velha",
-    "Arménia": "ereva",
-    "Áustria": "viena",
-    "Azerbaijão": "baku",
-    "Bielorússia": "minsk",
-    "Bélgica": "bruxelas",
-    "Bósnia-Herzegovina": "saravejo",
-    "Bulgária": "sofia",
-    "República Checa": "praga",
-    "Chipre": "nicosia",
-    "Dinamarca": "copenhaga",
-    "Eslováquia": "bratislava",
-    "Eslovénia": "liubliana",
-    "Espanha": "madrid",
-    "Estónia": "tallinn",
-    "Finlândia": "helsinquia",
-    "França": "paris",
-    "Grécia": "atenas",
-    "Geórgia": "tbilisi",
-    "Hungria": "budapeste",
-    "Irlanda": "dublin",
-    "Islândia": "reiquiavique",
-    "Itália": "roma",
-    "Letônia": "riga",
-    "Liechtenstein": "vaduz",
-    "Lituânia": "vilnius",
-    "Luxemburgo": "luxemburgo",
-    "República da Macedónia": "escopia",
-    "Malta": "valetta",
-    "Moldávia": "chisinau",
-    "Mónaco": "cidade do monaco",
-    "Montenegro": "podgorica",
-    "Noruega": "oslo",
-    "Países Baixos": "amesterdao",
-    "Polónia": "varsovia",
-    "Portugal": "lisboa",
-    "Reino Unido": "londres",
-    "Romênia": "bucareste",
-    "Rússia": "moscovo",
-    "Turquia": "ancara",
-    "San Marino": "san marino",
-    "Sérvia": "belgrado",
-    "Suécia": "estocolmo",
-    "Suíça": "berna",
-    "Ucrânia": "kiev",
-    "Vaticano": "cidade do vaticano"
+    "Canadá": ["ottawa", "otava"],
+    "Estados Unidos da América": ["washington", "washington dc", "washington d.c.", "washington d c"],
+    "México": ["cidade do mexico"],
+    "Antígua e Barbuda": ["Saint John's", "saint johns", "saint john's", "sao joao"],
+    "Bahamas": ["nassau"],
+    "Barbados": ["bridgetown"],
+    "Belize": ["belmopan"],
+    "Costa Rica": ["São José", "sao jose"],
+    "Cuba": ["havana"],
+    "Dominica": ["roseau"],
+    "El Salvador": ["São Salvador", "sao salvador", "san salvador"],
+    "Granada": ["Saint George's", "saint george's", "saint georges", "sao jorge"],
+    "Guatemala": ["cidade da guatemala"],
+    "Haiti": ["Porto Príncipe", "porto principe"],
+    "Honduras": ["tegucigalpa"],
+    "Jamaica": ["kingston"],
+    "Nicarágua": ["managua"],
+    "Panamá": ["Cidade do Panamá", "cidade do panama"],
+    "República Dominicana": ["Santo Domingo", "santo domingo", "sao domingos"],
+    "Santa Lúcia": ["castries"],
+    "São Cristóvão e Nevis": ["basseterre"],
+    "São Vicente e Granadinas": ["kingstown"],
+    "Trinidad e Tobago": ["Porto de Espanha", "orto de espanha"]
 
 };
 
-var europe;
+var _europe = {
+
+    "Albânia": ["tirana"],
+    "Alemanha": ["berlim"],
+    "Andorra": ["andorra a velha", "andorra-a-velha"],
+    "Arménia": ["ereva", "erevan"],
+    "Áustria": ["viena", "vienna"],
+    "Azerbaijão": ["baku"],
+    "Bielorússia": ["minsk"],
+    "Bélgica": ["bruxelas"],
+    "Bósnia-Herzegovina": ["saravejo"],
+    "Bulgária": ["sofia"],
+    "República Checa": ["praga"],
+    "Chipre": ["nicosia"],
+    "Dinamarca": ["copenhaga"],
+    "Eslováquia": ["bratislava"],
+    "Eslovénia": ["liubliana"],
+    "Espanha": ["madrid"],
+    "Estónia": ["talim", "tallin", "taline"],
+    "Finlândia": ["helsinquia"],
+    "França": ["paris"],
+    "Grécia": ["atenas"],
+    "Geórgia": ["tbilisi"],
+    "Hungria": ["budapeste"],
+    "Irlanda": ["dublin"],
+    "Islândia": ["reiquiavique"],
+    "Itália": ["roma"],
+    "Letónia": ["riga"],
+    "Liechtenstein": ["vaduz"],
+    "Lituânia": ["vilnius"],
+    "Luxemburgo": ["luxemburgo"],
+    "República da Macedónia": ["escopia"],
+    "Malta": ["valeta"],
+    "Moldávia": ["quichinau", "quixinau", "chisinau"],
+    "Mónaco": ["cidade do monaco"],
+    "Montenegro": ["podgorica"],
+    "Noruega": ["oslo"],
+    "Países Baixos": ["amesterdao"],
+    "Polónia": ["varsovia"],
+    "Portugal": ["lisboa", "lisbon"],
+    "Reino Unido": ["londres", "london"],
+    "Romênia": ["bucareste"],
+    "Rússia": ["moscovo", "moscow"],
+    "Turquia": ["ancara"],
+    "San Marino": ["san marino"],
+    "Sérvia": ["belgrado"],
+    "Suécia": ["estocolmo"],
+    "Suíça": ["berna"],
+    "Ucrânia": ["kiev"],
+    "Vaticano": ["cidade do vaticano"]
+
+};
+
+var selectedCountries;
+var continent;
 var curr_country;
 var skips;
 var hits;
 var popoverTimeout;
 var helpGiven;
 
-function startGame() {
-
+function resetVariables(name) {
     hits = 0;
     skips = 0;
-    europe = Object.keys(_europe);
-    $("#startEurope").hide();
-    $("#tableEurope").removeClass("d-none");
+    switch (name) {
+        case "EU":
+            selectedCountries = Object.keys(_europe);
+            continent = _europe;
+            $(".my-container").addClass("gameEurope");
+            break;
+        case "CNA":
+            selectedCountries = Object.keys(_northcentralAmerica);
+            $(".my-container").addClass("gameCNA");
+            continent = _northcentralAmerica;
+            break;
+    }
+}
+
+function startGame(name) {
+
+    resetVariables(name);
+    $("#startGame").hide();
+    $("#table").removeClass("d-none");
     nextStep();
+}
+
+function restartGame() {
+
+    $(".my-container").removeClass("gameEurope");
+    $(".my-container").removeClass("gameCNA");
+
+    $("#help").removeClass("disabled");
+    $("#help").removeAttr("disabled");
+
+    $("#check").removeClass("disabled");
+    $("#check").removeAttr("disabled");
+
+    $("#skip").removeClass("disabled");
+    $("#skip").removeAttr("disabled");
+
+    $("#restart").addClass("d-none");
+    $("#table").addClass("d-none");    
+    $("#startGame").show();
+    $("#random_country").html("&nbsp;");
+
 }
 
 function nextStep() {
 
-    if (europe.length == 0) {
+    helpGiven = false;
+
+    if (selectedCountries.length == 0) {
 
         $("#help").addClass("disabled");
         $("#help").prop("disabled", "disabled");
+
         $("#check").addClass("disabled");
         $("#check").prop("disabled", "disabled");
+
         $("#skip").addClass("disabled");
         $("#skip").prop("disabled", "disabled");
+
+        $("#restart").removeClass("d-none");
         $("#random_country").html("&nbsp;");
-        $("#resultEurope").html("Acertaste em <span class=\"text-success\">" + hits + "</span> e saltaste <span class=\"text-danger\"> " + skips + "</span>.");
-       
+        $("#result").html("Acertaste em <span class=\"text-success\">" + hits + "</span> e saltaste <span class=\"text-danger\"> " + skips + "</span>.");
+
     } else {
 
-        helpGiven = false;
+        var rdm_i = Math.floor(Math.random() * selectedCountries.length);
 
-        var rdm_i = Math.floor(Math.random() * europe.length);
-
-        curr_country = europe[rdm_i];
+        curr_country = selectedCountries[rdm_i];
 
         $("#random_country").html(curr_country);
         $("#capital_input").val('');
 
         // unica maneira decente de avançar com esta merda
-        europe.splice(rdm_i, 1);
+        selectedCountries.splice(rdm_i, 1);
 
-        $("#resultEurope").html("&nbsp;");
+        $("#result").html("&nbsp;");
     }
 }
 
@@ -131,20 +193,15 @@ function checkAnswer() {
         input = input.substring(0, input.length() - 1);
     }
 
-    input = input.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace("-", " ");
+    input = input.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/-/g, " ");
 
-    if (input.toLowerCase() == _europe[curr_country]) {
+    if (continent[curr_country].includes(input.toLowerCase())) {
 
         hits++;
         nextStep();
 
-    } else {
-
-        $("#capital_input").popover('show');
-        clearTimeout(popoverTimeout);
-        popoverTimeout = setTimeout(hidePop, 2000);
-
-    }
+    } 
+    // else ignore
 }
 function hidePop() {
     $('#capital_input').popover('hide');
@@ -158,7 +215,9 @@ function giveHelp() {
     var i;
 
     if (!helpGiven) {
-        for (i = 0; i < _europe[curr_country].length; i++) {
+
+        var capital = continent[curr_country][0];
+        for (i = 0; i < capital.length; i++) {
             chars.push("_");
         }
 
@@ -168,7 +227,7 @@ function giveHelp() {
             var rdm_i = Math.floor(Math.random() * chars.length);
             if (!charIndexes.includes(rdm_i)) {
                 charIndexes.push(rdm_i);
-                chars[rdm_i] = _europe[curr_country].charAt(rdm_i);
+                chars[rdm_i] = capital.charAt(rdm_i);
             }
         }
 
@@ -179,7 +238,7 @@ function giveHelp() {
                 hint += chars[i] + " ";
         }
 
-        $("#resultEurope").text(hint.charAt(0).toUpperCase() + hint.slice(1));
+        $("#result").text(hint.charAt(0).toUpperCase() + hint.slice(1));
 
         helpGiven = true;
     }
